37ad1730c19935afa4a12075527f72824c79ad90
ASstudio7 <bacon42461@gmail.com>
2025-04-15 15:57:09 +0100

    commit synt2x

--- transactions.py
+++ transactions.py
@@ -1,86 +1,87 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session, flash, redirect, make_response, abort
-from datetime import datetime, timedelta
-
-from app.models.user_transactions import UserTransaction
-from app.util import auth
 from app.extensions import db
+from app.models.user_transactions import UserTransaction
 from app.models.user import User
 from app.models.groups import Group
 from app.models.asset import Asset
 from app.enums.TransactionType import TransactionType
+import math
 
-TransactionsRoute = Blueprint('transactions', __name__, url_prefix='/transactions')
+def CreateTransaction(
+    Reciever : User | Group = None,
+    Sender : User | Group = None,
+    CurrencyAmount : int = 0,
+    CurrencyType : int = 0, # 0 = Robux, 1 = Tix
+    TransactionType : TransactionType = TransactionType.Purchase,
+    AssetId : int = None,
+    CustomText : str = None
 
-CategoryToEnum = {
-    "purchase": TransactionType.Purchase,
-    "sale": TransactionType.Sale,
-    "group-payout": TransactionType.GroupPayout,
-    "stipends": TransactionType.BuildersClubStipend,
-}
+) -> UserTransaction | None:
+    """
+        Creates a transaction between two users or group.
+    
+        Reciever: Defaults to UserId 1 (Roblox),
+        Sender: Defaults to UserId 1 (Roblox)
 
-@TransactionsRoute.route("/", methods=["GET"])
-@auth.authenticated_required
-def TransactionsPage():
-    AuthenticatedUser : User = auth.GetCurrentUser()
+        Note: If Reciever and Sender are both None an exception will be raised.
+    """
 
-    CategoryArg = request.args.get('category', default="purchase", type=str)
-    if CategoryArg not in CategoryToEnum:
-        Category : TransactionType = TransactionType.Purchase
-    else:
-        Category : TransactionType = CategoryToEnum[CategoryArg]
-        
-    PageNumber = request.args.get('page', default=1, type=int)
-    if PageNumber < 1:
-        PageNumber = 1
+    if Reciever is None and Sender is None:
+        raise Exception("Reciever and Sender cannot both be None.")
+    
+    if Reciever is None:
+        Reciever = User.query.filter_by(id=1).first()
+    if Sender is None:
+        Sender = User.query.filter_by(id=1).first()
+    
+    NewTransaction : UserTransaction = UserTransaction(
+        reciever_id = Reciever.id,
+        reciever_type = 0 if isinstance(Reciever, User) else 1,
+        sender_id = Sender.id,
+        sender_type = 0 if isinstance(Sender, User) else 1,
 
-    TransactionQuery = UserTransaction.query.filter_by( transaction_type = Category)
-    CategoryQueryDict = {
-        TransactionType.Purchase: lambda queryObj: queryObj.filter_by(
-            sender_id = AuthenticatedUser.id,
-            sender_type = 0
-        ),
-        TransactionType.Sale: lambda queryObj: queryObj.filter_by(
-            reciever_id = AuthenticatedUser.id,
-            reciever_type = 0
-        ),
-        TransactionType.GroupPayout: lambda queryObj: queryObj.filter_by(
-            reciever_id = AuthenticatedUser.id,
-            reciever_type = 0
-        ),
-        TransactionType.BuildersClubStipend: lambda queryObj: queryObj.filter_by(
-            reciever_id = AuthenticatedUser.id,
-            reciever_type = 0
-        ),
-    }
-    TransactionQuery = CategoryQueryDict[Category](TransactionQuery)
-    TransactionQuery = TransactionQuery.order_by(UserTransaction.created_at.desc())
-    TransactionQuery = TransactionQuery.paginate( page=PageNumber, per_page=15, error_out=False )
+        currency_amount = CurrencyAmount,
+        currency_type = CurrencyType,
+        transaction_type = TransactionType,
+        custom_text = CustomText,
 
-    FormattedTransactions = []
-    for Transaction in TransactionQuery.items:
-        Transaction : UserTransaction = Transaction
-        TransactionInfo = {}
-        TransactionInfo["source"] = {
-            "id": Transaction.sender_id if Transaction.sender_id != AuthenticatedUser.id or Transaction.sender_type != 0 else Transaction.reciever_id,
-            "type": Transaction.sender_type if Transaction.sender_id != AuthenticatedUser.id or Transaction.sender_type != 0 else Transaction.reciever_type, # VV I know this is bad but im too lazy to think of another way to do it
-            "name": ( User.query.filter_by(id = Transaction.sender_id).first().username if Transaction.sender_type != 1 else Group.query.filter_by( id = Transaction.sender_id ).first().name ) if Transaction.sender_id != AuthenticatedUser.id or Transaction.sender_type != 0 else ( User.query.filter_by(id = Transaction.reciever_id).first().username if Transaction.reciever_type != 1 else Group.query.filter_by( id = Transaction.reciever_id ).first().name ),
-        }
-        TransactionInfo["currency_amount"] = Transaction.currency_amount
-        TransactionInfo["currency_type"] = Transaction.currency_type
-        TransactionInfo["created_at"] = Transaction.created_at.strftime("%d/%m/%Y %H:%M:%S UTC")
-        TransactionInfo["custom_text"] = Transaction.custom_text
-        if Transaction.assetId:
-            TransactionInfo["asset"] = {
-                "id": Transaction.assetId,
-                "name": Asset.query.filter_by(id = Transaction.assetId).first().name,
-            }
-        else:
-            TransactionInfo["asset"] = None
-        FormattedTransactions.append(TransactionInfo)
-
-    return render_template(
-        "transactions/transactions.html",
-        PageCategory = CategoryArg,
-        TransactionInfo = FormattedTransactions,
-        Pagination = TransactionQuery
+        asset_id = AssetId
     )
+    db.session.add(NewTransaction)
+    db.session.commit()
+
+    return NewTransaction
+
+def CreateTransactionForSale(
+    AssetObj : Asset,
+    PurchasePrice : int,
+    PurchaseCurrencyType : int, # 0 = Robux, 1 = Tix,
+    Seller : User | Group,
+    Buyer : User | Group,
+    ApplyTaxAutomatically : bool = True
+) -> tuple[UserTransaction, UserTransaction] | None:
+    """
+        Creates a transaction for a sale.
+    """
+
+    if ApplyTaxAutomatically:
+        PurchaseProfit : int = math.floor(PurchasePrice * 0.7) # 70% of the purchase price goes to the seller
+    else:
+        PurchaseProfit : int = PurchasePrice
+    
+    SellerTransaction : UserTransaction = CreateTransaction(
+        Reciever = Seller,
+        Sender = Buyer,
+        CurrencyAmount = PurchaseProfit,
+        CurrencyType = PurchaseCurrencyType,
+        TransactionType = TransactionType.Sale,
+        AssetId = AssetObj.id
+    )
+    BuyerTransaction : UserTransaction = CreateTransaction(
+        Reciever = Seller,
+        Sender = Buyer,
+        CurrencyAmount = PurchasePrice,
+        CurrencyType = PurchaseCurrencyType,
+        TransactionType = TransactionType.Purchase,
+        AssetId = AssetObj.id
+    )
+    return (SellerTransaction, BuyerTransaction)
